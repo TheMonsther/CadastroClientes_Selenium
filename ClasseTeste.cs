aa377527ebb9c2960d11b74e26c07113c71fc2bb
TheMonsther <omega.renan.rodrigues@gmail.com>
2021-01-08 11:17:21 -0300

    Elaboração de mais Cenários

--- ClasseTeste.cs
+++ ClasseTeste.cs
@@ -23,36 +23,110 @@ namespace CadastroClientes_Selenium
 
             Cenario1();
             Cenario2();
+            Cenario3();
+            Cenario4();
+            Cenario5();
         }
-        /*
-         * Dado
-         * Quando
-         * Então
-         */
+        //para todos os Cenários é assumido um e-mail válido sendo um e-mail que seja válido na expressão regular ^\w*(\.\w*)?@\w*+\.com$
 
         /*
          * Dado que eu esteja na tela de cadastro
          * E insira meu nome e sobrenome
          * E insira meu email válido
-         * E insira uma senha com mais de 8 caracteres
+         * E insira uma senha com 8 caracteres
          * Quando clicar em Cadastrar
          * Então a página deve exibir meu cadastro feito em uma tabela
          */
         public static void Cenario1()
         {
-            cadastroCliente.TxtNome.Text = "Roberto Golveia";
-            cadastroCliente.TxtEmail.Text = "roberto@gmail.com";
+            cadastroCliente.TxtNome.Text = "Nome Sobrenome";
+            cadastroCliente.TxtEmail.Text = "teste@gmail.com";
             cadastroCliente.TxtSenha.Text = "12345678";
             cadastroCliente.SetDatas();
             cadastroCliente.BtnCadastrar.Click();
             if(cadastroCliente.ValidaCadastro())
-            Console.WriteLine("Cenario encerrado com sucesso!");
-            else Console.WriteLine("Cenario encerrado com divergencia no resultado");
+            Console.WriteLine("\nCenario1 executado com sucesso!\n");
+            else Console.WriteLine("\nCenario1 executado com divergencia no resultado esperado!\n");
         }
 
+        /*
+         * Dado que eu esteja na tela de cadastro
+         * E insira meu nome
+         * E insira meu email válido
+         * E insira uma senha com 8 caracteres
+         * Quando clicar em Cadastrar
+         * Então a página deve não exibir meu cadastro feito em uma tabela
+         */
         public static void Cenario2()
         {
+            cadastroCliente.TxtNome.Text = "Nome";
+            cadastroCliente.TxtEmail.Text = "teste@gmail.com";
+            cadastroCliente.TxtSenha.Text = "12345678";
+            cadastroCliente.SetDatas();
+            cadastroCliente.BtnCadastrar.Click();
+            if (cadastroCliente.ValidaCadastro())
+                Console.WriteLine("\nCenario2 executado com divergencia no resultado esperado!\n");
+            else Console.WriteLine("\nCenario2 executado com sucesso!\n");
+        }
 
+        /*
+         * Dado que eu esteja na tela de cadastro
+         * E insira meu nome e sobrenome
+         * E insira meu email inválido
+         * E insira uma senha com 8 caracteres
+         * Quando clicar em Cadastrar
+         * Então a página deve não exibir meu cadastro feito em uma tabela
+         */
+        public static void Cenario3()
+        {
+            cadastroCliente.TxtNome.Text = "Nome Sobrenome";
+            cadastroCliente.TxtEmail.Text = "teste@gmail.co";
+            cadastroCliente.TxtSenha.Text = "12345678";
+            cadastroCliente.SetDatas();
+            cadastroCliente.BtnCadastrar.Click();
+            if (cadastroCliente.ValidaCadastro())
+                Console.WriteLine("\nCenario3 executado com divergencia no resultado esperado!\n");
+            else Console.WriteLine("\nCenario3 executado com sucesso!\n");
+        }
+
+        /*
+         * Dado que eu esteja na tela de cadastro
+         * E insira meu nome e sobrenome
+         * E insira meu email válido
+         * E insira uma senha com menos de 8 caracteres
+         * Quando clicar em Cadastrar
+         * Então a página deve não exibir meu cadastro feito em uma tabela
+         */
+        public static void Cenario4()
+        {
+            cadastroCliente.TxtNome.Text = "Nome Sobrenome";
+            cadastroCliente.TxtEmail.Text = "teste@gmail.com";
+            cadastroCliente.TxtSenha.Text = "1234567";
+            cadastroCliente.SetDatas();
+            cadastroCliente.BtnCadastrar.Click();
+            if (cadastroCliente.ValidaCadastro())
+                Console.WriteLine("\nCenario4 executado com divergencia no resultado esperado!\n");
+            else Console.WriteLine("\nCenario4 executado com sucesso!\n");
+        }
+
+        /*
+         * Dado que eu esteja na tela de cadastro
+         * E insira meu nome, sobrenome1 e sobrenome2
+         * E insira meu email grande válido
+         * E insira uma senha com mais de 8 caracteres e com caracateres especiais
+         * Quando clicar em Cadastrar
+         * Então a página deve não exibir meu cadastro feito em uma tabela
+         */
+        public static void Cenario5()
+        {
+            cadastroCliente.TxtNome.Text = "Nome Sobrenome1 Sobrenome2";
+            cadastroCliente.TxtEmail.Text = "testeemail.Nome.Sobrenome1.Sobrenome2@hotmail.com";
+            cadastroCliente.TxtSenha.Text = "Senha C0m Mais De 8 Caracteres !@#$%**--==";
+            cadastroCliente.SetDatas();
+            cadastroCliente.BtnCadastrar.Click();
+            if (cadastroCliente.ValidaCadastro())
+                Console.WriteLine("\nCenario5 executado com sucesso!\n"); 
+            else Console.WriteLine("\nCenario5 executado com divergencia no resultado esperado!\n");
         }
     }
 }
