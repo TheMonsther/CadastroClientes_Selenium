ce910495d3f406fce577578e011ff042feb30eb5
TheMonsther <omega.renan.rodrigues@gmail.com>
2021-01-08 07:50:34 -0300

    Commit para inicializar o projeto no git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadastroClientes_Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
